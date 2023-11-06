209fe010c47f3888b0e9a78c45cc6e50dac24afe
Shabnaz21 <shabnaz608@gmail.com>
2023-11-06 16:34:07 +0600

    Mongodb database connect & Get Food data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0s8hpk2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0s8hpk2.mongodb.net/SharePlate?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,10 +19,20 @@ const client = new MongoClient(uri, {
     }
 });
 
+// Connection Collection
+const foodCollection = client.db('SharePlate').collection('Foods')
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        app.get('/foods', async (req, res) => { 
+            const foodFind = foodCollection.find();
+            const result = await foodFind.toArray();
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
