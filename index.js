0a2f93c16e4382674ffddb20d6021726ff8a6954
Shabnaz21 <shabnaz608@gmail.com>
2023-11-08 02:29:49 +0600

    axios dependency add in cors middleware

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: [
+            'http://localhost:5173',
+        ],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
