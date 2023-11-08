f90ebcf4ccaa267f3f565324fa3e405d420db9b7
Shabnaz21 <shabnaz608@gmail.com>
2023-11-08 21:43:09 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors(
     {
         origin: [
-            'http://localhost:5173',
+            'https://share-plate-1d8b2.web.app',
+            'https://share-plate-1d8b2.firebaseapp.com'
+            // 'http://localhost:5173'
+            
         ],
         credentials: true
     }
@@ -17,30 +20,7 @@ app.use(cors(
 app.use(express.json());
 app.use(cookieParser());
 
-// middlewares by myself
 
-const gateman = async (req, res, next) => {
-    console.log('called', req.host, req.originalUrl);
-    next();
-}
-
-// verify
-const verifyToken = (req, res, next) => {
-    const token = req.cookies?.token;
-    console.log('token in the middleware', token);
-    // no token
-    if (!token) {
-        return res.status(401).send({ message: 'unauthorized access' })
-    }
-    // jwt verify
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({ message: 'unauthorized access' })
-        }
-        req.user = decoded;
-        next();
-    })
-}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0s8hpk2.mongodb.net/SharePlate?retryWrites=true&w=majority`;
@@ -63,25 +43,6 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-
-        // jwt
-
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'})
-            res.
-                cookie('token', token, {
-                    httpOnly: true,
-                    sameSite: 'none',
-                    secure: true,
-                })
-                .send({ success: true })
-        })
-        // logout
-        app.post('/logout', async (req, res) => {
-            const user = req.body;
-            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
-        })
 
         // All Foods
         app.post('/foods', async (req, res) => {
@@ -109,11 +70,12 @@ async function run() {
             if (foodName) {
                 queryObj.foodName = foodName;
             }
+            
             // sorting
             if (sortField && sortOrder) {
                 sortObj[sortField] = sortOrder
             }
-
+ 
             const foodFind = foodCollection.find(queryObj).skip(skip).limit(limit).sort(sortObj);
             const result = await foodFind.toArray();
             const total = await foodCollection.countDocuments()
@@ -123,26 +85,36 @@ async function run() {
             });
         })
 
-        app.get('/foods/:id', async (req, res) => {
+        app.get('/foods/:id',  async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await foodCollection.findOne(query);
             // console.log(result);
             res.send(result);
         })
+        app.delete('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id),
+            };
+            const result = await RequestCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
 
         //Request Part
-        app.post('/food-request', gateman,verifyToken, async (req, res) => {
+        app.post('/food-request', async (req, res) => {
             const foods = req.body;
             const result = await RequestCollection.insertOne(foods);
             res.send(result);
         })
 
-        app.get('/food-request', gateman, verifyToken,  async (req, res) => {
+        app.get('/food-request',  async (req, res) => {
             console.log('token owner info', req.user);
-            // if (req.user.email !== req.query.email) {
-            //     return res.status(403).send({ message: 'Forbidden access' })
-            // }
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
             let query = {};
             if (req.query?.email) {
                 query={email: req.query.email}
@@ -151,7 +123,7 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('/food-request/:id', gateman, verifyToken, async (req, res) => {
+        app.patch('/food-request/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const UpdatedFoodData = req.body;
@@ -164,7 +136,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/food-request/:id', gateman, verifyToken, async (req, res) => {
+        app.delete('/food-request/:id',  async (req, res) => {
             const id = req.params.id;
             const query = {
                 _id: new ObjectId(id),
